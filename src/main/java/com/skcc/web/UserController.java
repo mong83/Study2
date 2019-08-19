fe3c3b3808a1ec96a4cbd6eef58270e501d3e522
mong83 <06007sk@gmail.com>
2019-08-19 21:46:32 +0900

    반복주기3 1. DB 설정 추가 2.JPA를 사용한 테이블 생성 3. form.html 회원가입 4. updateform.html 개인정보수정(회원가입 form과 동일한 layout)

--- src/main/java/com/skcc/web/UserController.java
+++ src/main/java/com/skcc/web/UserController.java
@@ -1,29 +1,68 @@
 package com.skcc.web;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.skcc.domain.User;
+import com.skcc.domain.UserRepository;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 	
-	private List<User> users = new ArrayList<User>();
+	
+	@Autowired
+	private UserRepository userRepository;
+	
 
-	@PostMapping("/create")
+	@PostMapping("")
 	public String create(User user) {
 		
-		System.out.println("User :" + user);
-		users.add(user);
-		return "redirect:/list"; // UserController의 @GetMapping("/list") 호출
+		System.out.println("User :" + user);		
+		userRepository.save(user);
+		return "redirect:/users"; // UserController의 @GetMapping("/list") 호출
 	}
 	
-	@GetMapping("/list")
+	@GetMapping("")
 	public String list(Model model) {
-		model.addAttribute("users" , users);		
-		return "list";          // templates아래의 list.html 호출
+		model.addAttribute("users" , userRepository.findAll());		
+		return "/user/list";          // templates아래의 list.html 호출
+	}
+	
+	@GetMapping("/form")
+	public String form() {				
+		return "/user/form";
+	}
+	
+	@GetMapping("/{id}/form")
+	public String updateForm(@PathVariable Long id , Model model) {
+		
+		Optional<User> tempUser = userRepository.findById(id);
+		User user = tempUser.get();
+		
+		model.addAttribute("user" , user);
+		
+		return "/user/updateForm";
+	}
+	
+	@PutMapping("/{id}")
+	public String updateForm(@PathVariable Long id , User newUser) {
+		
+		Optional<User> tempUser = userRepository.findById(id);
+		User user = tempUser.get();
+		
+		user.update(newUser);
+		userRepository.save(user);
+		
+		return "redirect:/users";
+		
 	}
 }
